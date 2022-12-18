00d1aab888cae833c12225c6dc6579d61afbf00b
matcha1024 <pettyblue@proton.me>
2022-12-18 19:58:37 +0900

    Improve Startuptime

--- lua/plugins/lsp/list.lua
+++ lua/plugins/lsp/list.lua
@@ -1,43 +1,44 @@
 return {
-    ["williamboman/mason.nvim"] = {
-        opt = true,
-        setup = function()
-            require("utils.lazy_load").on_file_open("mason.nvim")
-        end,
-    },
+	["williamboman/mason.nvim"] = {
+		opt = true,
+		module = "mason.nvim",
+		setup = function()
+			require("utils.lazy_load").on_file_open("mason.nvim")
+		end,
+	},
 
-    ["williamboman/mason-lspconfig.nvim"] = {
-        opt = true,
-        setup = function()
-            require("utils.lazy_load").on_file_open("mason-lspconfig.nvim")
-        end,
-    },
+	["williamboman/mason-lspconfig.nvim"] = {
+		opt = true,
+		setup = function()
+			require("utils.lazy_load").on_file_open("mason-lspconfig.nvim")
+		end,
+	},
 
-    ["neovim/nvim-lspconfig"] = {
-        after = { "mason-lspconfig.nvim", "mason.nvim" },
-        config = function()
-            require("plugins.lsp.configs.setup")
-        end,
-    },
+	["neovim/nvim-lspconfig"] = {
+		after = { "mason-lspconfig.nvim", "mason.nvim" },
+		config = function()
+			require("plugins.lsp.configs.setup")
+		end,
+	},
 
-    ["glepnir/lspsaga.nvim"] = {
-        after = "mason.nvim",
-        branch = "main",
-        config = function()
-            require("plugins.lsp.configs.saga")
-            require("plugins.lsp.configs.diagnostics").setup()
-        end,
-        setup = function()
-            require("utils.mapping").set("saga")
-        end,
-    },
+	["glepnir/lspsaga.nvim"] = {
+		after = "mason.nvim",
+		branch = "main",
+		config = function()
+			require("plugins.lsp.configs.saga")
+			require("plugins.lsp.configs.diagnostics").setup()
+		end,
+		setup = function()
+			require("utils.mapping").set("saga")
+		end,
+	},
 
-    ["onsails/lspkind-nvim"] = {},
+	["onsails/lspkind-nvim"] = {},
 
-    ["jose-elias-alvarez/null-ls.nvim"] = {
-        after = "nvim-lspconfig",
-        config = function()
-            require("plugins.lsp.configs.null_ls")
-        end,
-    },
+	["jose-elias-alvarez/null-ls.nvim"] = {
+		after = "nvim-lspconfig",
+		config = function()
+			require("plugins.lsp.configs.null_ls")
+		end,
+	},
 }
--- lua/plugins/tool/list.lua
+++ lua/plugins/tool/list.lua
@@ -1,124 +1,125 @@
 return {
-    ["windwp/nvim-autopairs"] = {
-        event = { "InsertEnter", "CmdlineEnter" },
-        config = function()
-            require("nvim-autopairs").setup()
-        end,
-    },
+	["windwp/nvim-autopairs"] = {
+		event = { "InsertEnter", "CmdlineEnter" },
+		config = function()
+			require("nvim-autopairs").setup()
+		end,
+	},
 
-    ["L3MON4D3/LuaSnip"] = {
-        event = { "InsertEnter", "CmdlineEnter" },
-        config = function()
-            require("plugins.tool.configs.luasnip")
-        end,
-    },
+	["L3MON4D3/LuaSnip"] = {
+		event = { "InsertEnter", "CmdlineEnter" },
+		config = function()
+			require("plugins.tool.configs.luasnip")
+		end,
+	},
 
-    ["hrsh7th/nvim-cmp"] = {
-        after = { "LuaSnip", "nvim-autopairs" },
-        config = function()
-            require("plugins.tool.configs.cmp")
-        end,
-        requires = {
-            { "hrsh7th/cmp-nvim-lsp", after = "nvim-lspconfig" },
-            { "hrsh7th/cmp-path", after = "nvim-cmp" },
-            { "hrsh7th/cmp-buffer", after = "nvim-cmp" },
-            { "hrsh7th/cmp-cmdline", after = "nvim-cmp" },
-            { "saadparwaiz1/cmp_luasnip", after = "nvim-cmp" },
-        },
-    },
+	["hrsh7th/nvim-cmp"] = {
+		after = { "LuaSnip", "nvim-autopairs" },
+		config = function()
+			require("plugins.tool.configs.cmp")
+		end,
+		requires = {
+			{ "hrsh7th/cmp-nvim-lsp", after = "nvim-lspconfig" },
+			{ "hrsh7th/cmp-path", after = "nvim-cmp" },
+			{ "hrsh7th/cmp-buffer", after = "nvim-cmp" },
+			{ "hrsh7th/cmp-cmdline", after = "nvim-cmp" },
+			{ "saadparwaiz1/cmp_luasnip", after = "nvim-cmp" },
+		},
+	},
 
-    ["simrat39/symbols-outline.nvim"] = {
-        cmd = "SymbolsOutline",
-    },
+	["simrat39/symbols-outline.nvim"] = {
+		cmd = "SymbolsOutline",
+	},
 
-    ["numToStr/Comment.nvim"] = {
-        keys = { "gc", "gcc", "gbc" },
-        config = function()
-            require("Comment").setup({})
-        end,
-    },
+	["numToStr/Comment.nvim"] = {
+		keys = { "gc", "gcc", "gbc" },
+		config = function()
+			require("Comment").setup({})
+		end,
+	},
 
-    ["nvim-treesitter/nvim-treesitter"] = {
-        run = ":TSUpdate",
-        setup = function()
-            require("utils.lazy_load").on_file_open("nvim-treesitter")
-        end,
-        cmd = require("utils.lazy_load").treesitter_cmds,
-        config = function()
-            require("plugins.tool.configs.treesitter")
-        end,
-    },
+	["nvim-treesitter/nvim-treesitter"] = {
+		run = ":TSUpdate",
+		module = "nvim-treesitter",
+		setup = function()
+			require("utils.lazy_load").on_file_open("nvim-treesitter")
+		end,
+		cmd = require("utils.lazy_load").treesitter_cmds,
+		config = function()
+			require("plugins.tool.configs.treesitter")
+		end,
+	},
 
-    ["nvim-treesitter/nvim-treesitter-textobjects"] = {
-        after = "nvim-treesitter",
-    },
+	["nvim-treesitter/nvim-treesitter-textobjects"] = {
+		after = "nvim-treesitter",
+	},
 
-    ["nvim-telescope/telescope.nvim"] = {
-        cmd = "Telescope",
-        config = function()
-            require("plugins.tool.configs.telescope")
-        end,
-        setup = function()
-            require("utils.mapping").set("telescope")
-        end,
-        requires = {
-            "nvim-lua/popup.nvim",
-            "nvim-lua/plenary.nvim",
-        },
-    },
+	["nvim-telescope/telescope.nvim"] = {
+		cmd = "Telescope",
+		config = function()
+			require("plugins.tool.configs.telescope")
+		end,
+		setup = function()
+			require("utils.mapping").set("telescope")
+		end,
+		requires = {
+			"nvim-lua/popup.nvim",
+			"nvim-lua/plenary.nvim",
+		},
+	},
 
-    ["akinsho/nvim-toggleterm.lua"] = {
-        keys = "<C-\\>",
-        cmd = "ToggleTerm",
-        config = function()
-            require("plugins.tool.configs.toggleterm")
-        end,
-        setup = function()
-            require("utils.mapping").set("toggleterm")
-        end,
-    },
+	["akinsho/nvim-toggleterm.lua"] = {
+		keys = "<C-\\>",
+		cmd = "ToggleTerm",
+		config = function()
+			require("plugins.tool.configs.toggleterm")
+		end,
+		setup = function()
+			require("utils.mapping").set("toggleterm")
+		end,
+	},
 
-    ["kevinhwang91/nvim-hlslens"] = {
-        opt = true,
-        setup = function()
-            require("utils.lazy_load").on_file_open("nvim-hlslens")
-            require("utils.mapping").set("hlslens")
-        end,
-    },
+	["kevinhwang91/nvim-hlslens"] = {
+		opt = true,
+		setup = function()
+			require("utils.lazy_load").on_file_open("nvim-hlslens")
+			require("utils.mapping").set("hlslens")
+		end,
+	},
 
-    ["norcalli/nvim-colorizer.lua"] = {
-        opt = true,
-        setup = function()
-            require("utils.lazy_load").on_file_open("nvim-colorizer.lua")
-        end,
-        config = function()
-            require("plugins.tool.configs.colorizer")
-        end,
-    },
+	["norcalli/nvim-colorizer.lua"] = {
+		opt = true,
+		setup = function()
+			require("utils.lazy_load").on_file_open("nvim-colorizer.lua")
+		end,
+		config = function()
+			require("plugins.tool.configs.colorizer")
+		end,
+	},
 
-    ["phaazon/hop.nvim"] = {
-        keys = { "gh" },
-        cmd = { "HopWord", "HopChar1" },
-        config = function()
-            require("hop").setup({})
-        end,
-    },
+	["phaazon/hop.nvim"] = {
+		keys = { "gh" },
+		cmd = { "HopWord", "HopChar1" },
+		config = function()
+			require("hop").setup({})
+		end,
+	},
 
-    ["ggandor/lightspeed.nvim"] = {
-        keys = { "f", "F", "t", "T" },
-        config = function()
-            require("lightspeed").setup({})
-        end,
-    },
+	["ggandor/lightspeed.nvim"] = {
+		keys = { "f", "F", "t", "T" },
+		config = function()
+			require("lightspeed").setup({})
+		end,
+	},
 
-    ["tweekmonster/startuptime.vim"] = {
-        cmd = "StartupTime",
-    },
+	["dstein64/vim-startuptime"] = {
+		cmd = "StartupTime",
+	},
 
-    ["RRethy/vim-illuminate"] = {
-        event = "CursorHold",
-        config = function()
-            vim.g.Illuminate_delay = 1000
-        end,
-    },
+	["RRethy/vim-illuminate"] = {
+		event = "CursorHold",
+		config = function()
+			vim.g.Illuminate_delay = 1000
+		end,
+	},
 }
